d813b10e13fa5f614131430ef5767861f7429129
Kaelin Cai (Centific Technologies Inc) <v-kaelincai@microsoft.com>
2025-12-04 10:37:32 +0800

    delete

--- oswe-mini-m22a2s300/run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Run integration tests for v2
-export PYTHONPATH="$(pwd)"
-pytest -q
--- oswe-mini-m22a2s300/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python -m pip install -U pip
-python -m pip install -r requirements.txt
-echo 'Setup complete'
